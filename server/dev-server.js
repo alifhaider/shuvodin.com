3d728f55ecd5e4bdcba1dc8a8aa62d6a030823fd
alifhaider <alifhaider57@gmail.com>
2025-08-17 22:15:50 +0600

    instruction

--- server/dev-server.js
+++ server/dev-server.js
@@ -3,6 +3,11 @@ import { execa } from 'execa'
 if (process.env.NODE_ENV === 'production') {
 	await import('../server-build/index.js')
 } else {
+
+	// without nodejs debugger
+	// const command = 'tsx watch --clear-screen=false --ignore ".cache/**" --ignore "app/**" --ignore "vite.config.ts.timestamp-*" --ignore "build/**" --ignore "node_modules/**" --inspect ./index.js'
+
+	// with nodejs debugger
 	const command = 'tsx watch --clear-screen=false --ignore ".cache/**" --ignore "app/**" --ignore "vite.config.ts.timestamp-*" --ignore "build/**" --ignore "node_modules/**" ./index.js'
 	execa(command, {
 		stdio: ['ignore', 'inherit', 'inherit'],
