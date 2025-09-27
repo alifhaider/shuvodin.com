51882e524d4554e21b55959aa16777bfa335b933
alifhaider <alifhaider57@gmail.com>
2025-09-27 06:30:56 +0600

    fix: update settings link to point to the correct profile settings route

--- app/routes/profiles+/$username.tsx
+++ app/routes/profiles+/$username.tsx
@@ -427,7 +427,7 @@ export default function VendorRoute({
 
 						<div className="flex gap-3">
 							<Button asChild variant="outline" size="sm">
-								<Link to="/profile/settings">
+								<Link to="/settings/profile">
 									<Icon name="settings" className="mr-2 h-4 w-4" />
 									Settings
 								</Link>
